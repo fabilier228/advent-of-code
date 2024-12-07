5d3c6d22d474fd4fe6abc7383a303d991fdad7a0
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2024-12-07 09:47:51 +0100

    updated day3

--- 2024/day3/part1.py
+++ 2024/day3/part1.py
@@ -2,15 +2,12 @@ import re
 
 with open('input.txt', 'r') as reading_file:
     data: str = reading_file.read()
-# Example corrupted memory input
 corrupted_memory = data
 
 pattern = r"do\(\)\Wmul\((\d+),(\d+)\)"
 
-# Find all matches of the pattern
 matches = re.findall(pattern, corrupted_memory)
 
-# Calculate the sum of products for valid matches
 result_sum = sum(int(x) * int(y) for x, y in matches)
 
 print(result_sum)
